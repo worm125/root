6406c0e04a78e562b9fbb662fe03397d1f296520
Anshuman Mitra <anxu37@gmail.com>
2017-08-02 12:25:29 +0530

    Create Main.java

---
+++ LINK 2/World Cup cheers 2 - S Pattern/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+  public static void printM(int n) {
+      for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+          if((j==0 && i<=n/2) || (j==n-1 && i>=n/2) || (i==0 || i==n-1 || i==n/2))
+            System.out.print("S");
+          else
+            System.out.print("*");
+        }
+        System.out.print("\n");
+      }
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    printM(n);
+  }
+}
